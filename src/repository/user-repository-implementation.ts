c0a94bdddbd50ee5231b9a01b3f089089110a297
jjribeiro1 <jvsr.99@hotmail.com>
2023-11-22 21:45:21 -0300

    criação do repositório de usuarios

---
+++ src/repository/user-repository-implementation.ts
@@ -0,0 +1,40 @@
+import { prisma } from '../database';
+import { IUserRepository } from './user-repository-interface';
+import { SignUpInput } from '../interfaces/sign-up-input';
+import { User } from '../entities/User';
+
+export class UserRepositoryImpl implements IUserRepository {
+  private userSelect = {
+    id: true,
+    nome: true,
+    email: true,
+    senha: true,
+    data_criacao: true,
+    data_atualizacao: true,
+    ultimo_login: true,
+    telefones: true,
+  };
+
+  async create(data: SignUpInput): Promise<User> {
+    const { email, nome, senha, telefones } = data;
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        nome,
+        senha,
+        telefones: {
+          create: telefones,
+        },
+      },
+      select: this.userSelect,
+    });
+
+    return newUser;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await prisma.user.findUniqueOrThrow({ where: { id }, select: this.userSelect });
+    return user;
+  }
+}
---
+++ src/repository/user-repository-interface.ts
@@ -0,0 +1,7 @@
+import { SignUpInput } from '../interfaces/sign-up-input';
+import { User } from '../entities/User';
+
+export interface IUserRepository {
+  create(data: SignUpInput): Promise<User>;
+  findById(id: string): Promise<User>;
+}
