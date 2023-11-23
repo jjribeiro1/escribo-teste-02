5927161a4203326cc022c9c497a1763efe5b7879
jjribeiro1 <jvsr.99@hotmail.com>
2023-11-23 10:04:41 -0300

    montagem das rotas sign in e sign up

--- src/routes/sign-in.ts
+++ src/routes/sign-in.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import { SignInController } from '../controllers/sign-in/sign-in-controller';
+import { AuthServiceImpl } from '../services/authentication/auth-service-implementation';
+import { UserRepositoryImpl } from '../repository/user-repository-implementation';
+import { CryptographyImpl } from '../services/cryptography/cryptography-implementation';
+import { TokenServiceImpl } from '../services/token/token-service-implementation';
 
 export const signInRoute = Router();
 
-signInRoute.post('/sign-in', (req, res) => {
-  return res.sendStatus(200);
-});
+const userRepository = new UserRepositoryImpl();
+const cryptographyService = new CryptographyImpl();
+const tokenService = new TokenServiceImpl();
+const authService = new AuthServiceImpl(userRepository, cryptographyService, tokenService);
+const signInController = new SignInController(authService);
+
+signInRoute.post('/sign-in', signInController.handler);
--- src/routes/sign-up.ts
+++ src/routes/sign-up.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import { SignUpController } from '../controllers/sign-up/sign-up-controller';
+import { SignUpServiceImpl } from '../services/sign-up/sign-up-implementation';
+import { UserRepositoryImpl } from '../repository/user-repository-implementation';
+import { CryptographyImpl } from '../services/cryptography/cryptography-implementation';
+import { TokenServiceImpl } from '../services/token/token-service-implementation';
 
 export const signUpRoute = Router();
 
-signUpRoute.post('/sign-up', (req, res) => {
-  return res.sendStatus(201);
-});
+const userRepository = new UserRepositoryImpl();
+const cryptographyService = new CryptographyImpl();
+const tokenService = new TokenServiceImpl();
+const signUpService = new SignUpServiceImpl(userRepository, cryptographyService, tokenService);
+const signUpController = new SignUpController(signUpService);
+
+signUpRoute.post('/sign-up', signUpController.handler);
